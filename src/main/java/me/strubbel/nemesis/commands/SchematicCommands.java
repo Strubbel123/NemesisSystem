1d9c8bc772cb1a9e18201134523f4be0c77c0e83
Luca Güttinger <lucagg59@gmail.com>
2016-12-25 15:58:20 +0100

    add Probe permission to schematic commands.

---
+++ src/main/java/me/strubbel/nemesis/commands/SchematicCommands.java
@@ -0,0 +1,64 @@
+package me.strubbel.nemesis.commands;
+
+import org.bukkit.entity.Player;
+
+import de.pro_crafting.commandframework.Command;
+import de.pro_crafting.commandframework.CommandArgs;
+import net.md_5.bungee.api.ChatColor;
+
+public class SchematicCommands {
+	@Command(name = "/schematic.load",aliases = {"schem.load"}, inGameOnly=true)
+    public void schemLoad(CommandArgs args) {
+		Player p = args.getPlayer();
+		if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+			if (args.length() > 0) {
+				p.performCommand(args.getCommand().toString()+" pr/"+args.getArgs());
+			}
+		}
+	}
+	
+	@Command(name = "/schematic.save",aliases = {"schem.save"}, inGameOnly=true)
+    public void schemSave(CommandArgs args) {
+		Player p = args.getPlayer();
+		if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+			if (args.length() > 0) {
+				if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+					if (args.length() > 0) {
+						p.performCommand(args.getCommand().toString()+" pr/"+args.getArgs());
+					}
+				}
+			}
+		}
+	}
+
+	@Command(name = "/schematic.list",aliases = {"schem.list"}, inGameOnly=true)
+    public void schemList(CommandArgs args) {
+		Player p = args.getPlayer();
+		if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+			if (args.length() > 0) {
+				if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+					if (args.length() > 0) {
+						p.sendMessage(ChatColor.RED+"Funktion bald verfügbar!");
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	@Command(name = "/schematic.delete",aliases = {"schem.delete"}, inGameOnly=true)
+    public void schemDelete(CommandArgs args) {
+		Player p = args.getPlayer();
+		if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+			if (args.length() > 0) {
+				if ((!(args.getPlayer().hasPermission("nemesis.schematic.member") && args.getPlayer().hasPermission("nemesis.schematic.pr"))) || (!args.getPlayer().isOp())) {
+					if (args.length() > 0) {
+						p.performCommand(args.getCommand().toString()+" pr/"+args.getArgs());
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+}
